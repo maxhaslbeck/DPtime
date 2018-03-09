581cf87aac65e25646b6a8fb354df51ee3000370
Simon Wimmer <wimmers@in.tum.de>
2018-03-09 11:53:21 +0100

    Simplified some proofs

--- DP.thy
+++ DP.thy
@@ -2,6 +2,8 @@ theory DP
 imports "../Imperative_HOL_Time/SepLogicTime/SepAuto" "../Imperative_HOL_Time/SepLogicTime/Asymptotics_1D"
 begin
 
+no_notation Ref.update ("_ := _" 62)
+
 (* auxiliaries *)
 
 
@@ -59,21 +61,14 @@ setup {* register_wellform_data ("fib_fun i xs", ["i < length xs"]) *}
 
 
 lemma fib_fun_stays[rewrite]: "n < length xs \<Longrightarrow> xs ! n \<noteq> None \<Longrightarrow> (fst (fib_fun n xs)) = xs"
-  apply(induct rule: fib_fun.induct) apply auto done
+  by(induction rule: fib_fun.induct) auto
 
 lemma fib_fun_length [rewrite]: "i < length xs \<Longrightarrow> length (fst (fib_fun i xs)) = length xs"
-@proof @fun_induct "fib_fun i xs"  @with
-  @subgoal "(i = 0, xs = xs)"
-    @case "xs ! 0 = None"
-  @endgoal
-  @subgoal "(i = Suc 0, xs = xs)"
-    @case "xs ! Suc 0 = None"
-  @endgoal
-  @subgoal "(i = Suc (Suc n), xs = xs)"
-    @unfold "fib_fun (Suc (Suc n)) xs"
-    @case "xs ! (Suc (Suc n)) \<noteq> None"
-  @endgoal @end
-@qed
+  by (induction i xs rule: fib_fun.induct) (auto split: option.split prod.split simp: Let_def)
+
+lemma consistent_list_update:
+  "consistent_list (xs[i:= Some v])" if "v = fib i" "consistent_list xs" "i < length xs"
+  using that by (auto simp add: consistent_list_def nth_list_update)
 
 (* the statemet of this lemma should be independent of the function (here fib) that is computed,
   also the structure of the proof should be the same, only potentially more recursive calls
@@ -83,42 +78,34 @@ lemma fib_fun_effect: "i < length xs \<Longrightarrow> consistent_list xs
       \<Longrightarrow> (fst (fib_fun i xs)) ! i = Some (fib i) \<and> snd (fib_fun i xs) = fib i \<and> consistent_list (fst (fib_fun i xs))"
 proof (induction i xs rule: fib_fun.induct)
   case (1 xs)
-  then show ?case apply(auto simp: consistent_list_def fib_fun_length split: option.split prod.split)
-    by (metis "1.prems"(1) One_nat_def fib.simps(1) nth_list_update option.inject)
+  then show ?case
+    by (auto simp: consistent_list_def fib_fun_length nth_list_update split: option.split)
 next
   case (2 xs)
-  then show ?case apply(auto simp: consistent_list_def fib_fun_length split: option.split prod.split)
-    by (metis One_nat_def fib.simps(2) nth_list_update option.inject)    
+  then show ?case
+    by (auto simp: consistent_list_def fib_fun_length nth_list_update split: option.split)
 next
   case (3 n xs)
-  show ?case 
+  show ?case
   proof (cases "xs ! Suc (Suc n) = None")
     case True  
+
     let ?xs' = "fst (fib_fun (Suc n) xs)"
     from True 3 have "?xs' ! Suc n = Some (fib (Suc n))"
         and a: "snd (fib_fun (Suc n) xs) = fib (Suc n)"
-        and c: "consistent_list ?xs'" by auto
+        and "consistent_list ?xs'" by auto
 
     let ?xs'' = "fst (fib_fun n ?xs')"
- 
-    from True c 3(2)[of None "fib_fun (Suc n) xs" "fst (fib_fun (Suc n) xs)"]
-    have "n < length (fst (fib_fun (Suc n) xs)) \<Longrightarrow> ?xs'' ! n = Some (fib n) \<and> snd (fib_fun n ?xs') = fib n \<and>
-      consistent_list ?xs''"
-      using prod.collapse by fastforce
-    with 3(3) have "?xs'' ! n = Some (fib n)" and b: "snd (fib_fun n ?xs') = fib n" and c'': "consistent_list ?xs''"
-      by (auto simp: fib_fun_length)      
+    from 3(2)[OF HOL.refl True HOL.refl prod.collapse] \<open>consistent_list ?xs'\<close> 3(3) have
+      "?xs'' ! n = Some (fib n)" and b: "snd (fib_fun n ?xs') = fib n" and "consistent_list ?xs''"
+      by (auto simp: fib_fun_length)
 
-    have c''': "consistent_list (?xs''[Suc (Suc n) := Some (fib (Suc (Suc n)))] )"
-      using c'' unfolding consistent_list_def apply auto
-        subgoal for i y apply(cases "i=Suc (Suc n)") by auto done
+    then have c: "consistent_list (?xs''[Suc (Suc n) := Some (fib (Suc (Suc n)))] )"
+      using 3(3) by (auto simp: fib_fun_length intro: consistent_list_update)
 
-    have t: "length ?xs'' = length xs" using 3(3) by (auto simp: fib_fun_length)
-    thm prod_def
-    from True show ?thesis apply(auto simp: split_beta split: option.split)
-      subgoal using t by (metis "3.prems"(1) a b fst_conv nth_list_update_eq) 
-      subgoal using t by (metis a b snd_conv) 
-      using c''' apply(auto simp add: a b) by (metis fst_conv) 
-    
+    from True show ?thesis
+      apply(auto simp: Let_def split_beta 3 split: option.split)
+      using c 3(3) by (auto simp: a b fib_fun_length)
   next
     case False
     with 3(3-4) show ?thesis unfolding consistent_list_def by auto
@@ -188,14 +175,14 @@ fun comcost :: "nat \<Rightarrow> nat" where
   "comcost n = 6"
 declare comcost.simps [rewrite]
 
-lemma comcost_ub: "comcost n \<le> 6" apply(induct n rule: comcost.induct) by auto
+lemma comcost_ub: "comcost n \<le> 6" by auto
 
 
 subsubsection "the potential for the memoization Table"
 
 definition memtableP :: " nat option list \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
   "memtableP xs A B = sum (\<lambda>i. case xs!i of None \<Rightarrow> comcost i | Some _ \<Rightarrow> 0) {A..<B}"
- 
+
 lemma empty_ub[rewrite]: "memtableP (replicate x None) 0 x = x * 6"
   unfolding memtableP_def by auto  
 (*
@@ -227,7 +214,7 @@ lemma memtableP_pays: "i<length xs \<Longrightarrow> xs ! i = None \<Longrightar
   unfolding memtableP_def by auto
 
 lemma memtableP_doesnt_pay: "i<length xs \<Longrightarrow> consistent_list xs \<Longrightarrow> memtableP (fst (fib_fun i xs)) i (Suc i) = 0"
-  unfolding memtableP_def apply auto using fib_fun_effect by auto 
+  unfolding memtableP_def using fib_fun_effect by auto 
 
 (* Key result for memtableP: if xs ! i = None, then updating xs ! i to Some v allows
    extracting comcost i credit from the potential. *)
@@ -245,7 +232,7 @@ proof -
 qed 
 
 
-lemma recombine[rewrite_back]: assumes "A \<le> i " "  i < B " " B \<le> length xs " " xs ! i = None"
+lemma recombine[rewrite_back]: assumes "A \<le> i" "i < B" "B \<le> length xs" "xs ! i = None"
     "consistent_list xs"
   shows "memtableP (fst (fib_fun i xs)) A B = memtableP (fst (fib_fun i xs)) A i + memtableP xs (Suc i) B"
 proof -
